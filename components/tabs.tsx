b0c064c1392262ed3f27b6f472fba23b15ab3729
Noah <noahpoul@live.ca>
2025-06-10 22:09:42 -0400

    notif style

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -14,9 +14,23 @@ const MailIconWithBadge = ({ color, size, badgeCount }: { color: string; size: n
   return (
     <View className="relative w-15 h-6 items-center justify-center">
       <Ionicons name="mail-outline" size={size} color={color} />
-      {badgeCount > 0 && color=='gray' && (
+      {badgeCount > 0 && color == 'gray' && (
         // <View className="absolute top-0 right-0 inline-flex items-center justify-center w-4 h-4  bg-red-500 rounded-full"></View>
-        <View className="absolute top-0 right-0 inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-500 rounded-full">
+        <View
+          style={{
+            position: 'absolute',
+            top: -5,
+            right: -7,
+            backgroundColor: '#ef4444',
+            borderRadius: 8,
+            minWidth: 16,
+            height: 16,
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingLeft: 3,
+            paddingRight: 4,
+          }}
+          className="text-xs font-bold text-white ">
           {badgeCount > 99 ? '99+' : badgeCount.toString()}
         </View>
       )}
@@ -25,101 +39,101 @@ const MailIconWithBadge = ({ color, size, badgeCount }: { color: string; size: n
 };
 
 function TabNavigation() {
-    const { user } = useAuth();
-    const { client } = useClientWithUserID(user?.id || "");
-    const [messageNotificationCount, setMessageNotificationCount] = useState(0);
-    
-    // Fetch message notifications count
-    useEffect(() => {
-      const fetchNotificationCount = async () => {
-        if (client?.id) {
-          try {
-            const messageNotifications = await getMessageNotifications(client.id);
-            setMessageNotificationCount(messageNotifications.length);
-          } catch (error) {
-            console.error('Error fetching notification count:', error);
-            setMessageNotificationCount(0);
-          }
-        } else {
+  const { user } = useAuth();
+  const { client } = useClientWithUserID(user?.id || "");
+  const [messageNotificationCount, setMessageNotificationCount] = useState(0);
+
+  // Fetch message notifications count
+  useEffect(() => {
+    const fetchNotificationCount = async () => {
+      if (client?.id) {
+        try {
+          const messageNotifications = await getMessageNotifications(client.id);
+          setMessageNotificationCount(messageNotifications.length);
+        } catch (error) {
+          console.error('Error fetching notification count:', error);
           setMessageNotificationCount(0);
         }
-      };
+      } else {
+        setMessageNotificationCount(0);
+      }
+    };
 
-      fetchNotificationCount();
-      
-      // // Optional: Set up an interval to periodically check for new notifications
-      // const interval = setInterval(fetchNotificationCount, 30000); // Check every 30 seconds
-      
-      // return () => clearInterval(interval);
-    }, [client?.id]);
-    
-    return (
-      <Tabs 
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: '#16a34a', 
-          tabBarInactiveTintColor: 'gray',
+    fetchNotificationCount();
+
+    // // Optional: Set up an interval to periodically check for new notifications
+    // const interval = setInterval(fetchNotificationCount, 30000); // Check every 30 seconds
+
+    // return () => clearInterval(interval);
+  }, [client?.id]);
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#16a34a',
+        tabBarInactiveTintColor: 'gray',
+      }}
+    >
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: '',//'About', 
+          href: !user ? `/about` : null,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+          tabBarStyle: user ? { display: 'none' } : undefined,
         }}
-      >
-        <Tabs.Screen 
-          name="about" 
-          options={{ 
-            title: '',//'About', 
-            href: !user ? `/about` : null,
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home-outline" size={size} color={color} />
-            ),
-            tabBarStyle: user ? { display: 'none' } : undefined,
-          }} 
-        />
-        <Tabs.Screen 
-          name="gear" 
-          options={{ 
-            title: '',//'Browse',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="search-outline" size={size} color={color} />
-            ),
-          }} 
-        />
-        <Tabs.Screen 
-          name="messages" 
-          options={{ 
-            title: '',//'Messages',  
-            href: user ? `/messages` : null,
-            tabBarIcon: ({ color, size }) => (
-              <MailIconWithBadge 
-                color={color} 
-                size={size} 
-                badgeCount={messageNotificationCount} 
-              />
-            ),
-            tabBarStyle: !user ? { display: 'none' } : undefined,
-          }} 
-        />
-        <Tabs.Screen 
-          name="profile" 
-          options={{ 
-            title: '',//'Profile', 
-            href: user ? `/profile/${client?.id}` : null,
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="person-outline" size={size} color={color} />
-            ),
-            //tabBarStyle: !user ? { display: 'none' } : undefined,
-          }} 
-        />
-        <Tabs.Screen 
-          name="auth" 
-          options={{ 
-            title: '',//'Login', 
-            href: !user ? `/auth` : null,
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="person-outline" size={size} color={color} />
-            ),
-            //tabBarStyle: user ? { display: 'none' } : undefined,
-          }} 
-        />
-      </Tabs>
-    );
-  }
+      />
+      <Tabs.Screen
+        name="gear"
+        options={{
+          title: '',//'Browse',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="messages"
+        options={{
+          title: '',//'Messages',  
+          href: user ? `/messages` : null,
+          tabBarIcon: ({ color, size }) => (
+            <MailIconWithBadge
+              color={color}
+              size={size}
+              badgeCount={messageNotificationCount}
+            />
+          ),
+          tabBarStyle: !user ? { display: 'none' } : undefined,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: '',//'Profile', 
+          href: user ? `/profile/${client?.id}` : null,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+          //tabBarStyle: !user ? { display: 'none' } : undefined,
+        }}
+      />
+      <Tabs.Screen
+        name="auth"
+        options={{
+          title: '',//'Login', 
+          href: !user ? `/auth` : null,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+          //tabBarStyle: user ? { display: 'none' } : undefined,
+        }}
+      />
+    </Tabs>
+  );
+}
 
 export default TabNavigation;
